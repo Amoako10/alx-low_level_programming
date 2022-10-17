e60e7a0d023ead18778a5c4c21ec12f5fdc5a3c2
Amoako10 <eamoako89@yahoo.com>
2022-10-17 16:00:03 -0700

    print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,7 +4,6 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int n;
@@ -12,10 +11,10 @@ int main(void)
 	for (n = 0 ; n < 16 ; n++)
 	{
 		if (n < 10)
-		{ 
+		{
 			putchar('0' + n);
 		}
-		else 
+		else
 		{
 			putchar(87 + n);
 		}
