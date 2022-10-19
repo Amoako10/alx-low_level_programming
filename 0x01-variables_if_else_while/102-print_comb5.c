2ce43a2f7b55d5f278eadc6636b61142a92867c8
Amoako10 <eamoako89@yahoo.com>
2022-10-19 07:57:53 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -13,7 +13,7 @@ int main(void)
 	int bas;
 
 	for ( tho = 0 ; tho < 10 ; tho++)
-	{ 
+	{
 		for (hun = 0 ; hun <= 10 ; hun++)
 		{
 			for (ten = 0 ; ten < 10 ; ten++)
