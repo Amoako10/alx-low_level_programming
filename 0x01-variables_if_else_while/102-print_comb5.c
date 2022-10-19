37d62a4c93ce1a3279c88dbfbabed754c915494c
Amoako10 <eamoako89@yahoo.com>
2022-10-19 12:34:45 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,36 +2,32 @@
 /**
  *  main - Entry - point
  *
- *  Return: Always 0 
+ *  Return: Always 0 if successful 
  */
 
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+	int i, j;
 
-for ( tho = 0 ; tho < 10 ; tho++)
+	for (i = 0; i < 100; i++)
 	{
-		for (ten = 0 ; ten < 10 ; ten++)
+		for (j = 0; j < 100; j++)
 		{
-			for ( bas = 0 ; bas < 10 ; bas++)
+			if (i < j)
 			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
-					putchar(32);
+					putchar(' ');
 				}
-				bas++;
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
-}
+}			       	
