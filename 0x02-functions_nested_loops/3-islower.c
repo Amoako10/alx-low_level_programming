57e326c878c69049ec90ef098aca30f3fc81e6ff
Amoako10 <eamoako89@yahoo.com>
2022-10-19 15:48:31 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _fslower - checks if a character is lowercase
+ * @c: the character
+ * Return: 1 if letter is lowercase, 0 if not
+ */
+int _inslower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
