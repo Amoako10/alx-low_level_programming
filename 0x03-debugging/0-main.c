c18faa22cb6068e22437ec6194f660937a94b243
Amoako10 <eamoako89@yahoo.com>
2022-10-20 07:54:30 -0700

    0 Task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * * main - tests function that prints if integer is positive or negative
- *  * Return: 0
+ * main - Test Function for positive or negative 
+ * Return: 0
  */
 
 int main(void)
@@ -9,7 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 	return (0);
 }
