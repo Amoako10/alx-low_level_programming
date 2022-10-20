3a9d4c813c78f7cb642626f588cd3a4043c8e327
Amoako10 <eamoako89@yahoo.com>
2022-10-20 15:48:15 -0700

    2 Task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - casues an infinite loop
- * Returen: 0
+ * main - causes an infinite loop
+ * Return 0
  */
 
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("Infinte loop incoming :(\n");
 	i = 0;
 
-	/*
+	/**
 	 * while (i < 10)
 	 * {
-	 *	 putchar(i);
+	 * 	putchar (i);
 	 * }
 	 */
 
-	printf("Infinite loop avoided! \\o/\n");
-
+	printf("Infinite loop avoided: \\o/\n");
 	return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "mian.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
