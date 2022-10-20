475a5aa97c6c8ce5382b8cb93bc33c32a22cdaae
Amoako10 <eamoako89@yahoo.com>
2022-10-19 17:25:06 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,13 @@
  * isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return: 1 if character is a letter,0 otherwise
+ * Return: 1 (True) else 0 (False)
  */
-int _isalpha(int c)
+int _isalpha(char c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
