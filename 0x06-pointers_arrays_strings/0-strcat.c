ff771e996813ff6cbaca7fb445e40ff78d4bd289
Amoako10 <eamoako89@ya89@yahoo.com>
2022-10-26 16:02:22 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++]
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (i = 0; dest[i] != '\0'; i++)
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
 	return (dest);
 }
