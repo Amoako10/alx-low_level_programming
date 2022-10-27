b36f9f62a38bb80994a5fcbd0990d590493c1693
Amoako10 <eamoako89@ya89@yahoo.com>
2022-10-27 16:12:14 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,46 @@
 #include "main.h"
-#include <stdio.h>
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
- * infinite_add - add 2 strings
- * @n1: string1
- * @n2: string2
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base
+ * add_strings - Adds the numbers stored in two strings
+ * @n1: The string containing the first number to be added
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result
+ * @r_index: The current index of the buffer
+ * Return: If r can store the sum - a pointer to the result
+ * If r cannot store the sum - 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
+	int num, tens = 0;
 
-	while (n1[a_len] != '\0')
-		a_len++;
-	while (n2[b_len] != '\0')
-		b_len++;
-	if (a_len > b_len)
-		biggest = a_len;
-	else
-		biggest = b_len;
-	if ((biggest + 1) >= size_r)
-		return (0);
-	r[biggest + 1] = '\0';
-
-	while (biggest >= 0)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
-		if (a_len > 0 && b_len > 0)
-			sum = a + b + carry;
-		else if (a_len < 0 && b_len > 0)
-			sum = b + carry;
-		else if (a_len > 0 && b_len < 0)
-			sum = a + carry;
-		else
-			sum = carry;
-
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
-		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[biggest] = sum;
-		a_len--;
-		b_len--;
-		biggest--;
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
 	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+	else if (tens && r_index < 0)
+		return (0);
+	return (r + r_index + 1);
 }
