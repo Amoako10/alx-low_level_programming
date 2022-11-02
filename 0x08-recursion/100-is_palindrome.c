1239d8ccf1ffc2817d657493c767c55d4c144b4c
Amoako10 <eamoako89@ya89@yahoo.com>
2022-11-02 05:39:15 -0700

    task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int is_palindrome(char *s)
+{
+	int 1;
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
+}
