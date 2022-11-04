70e776f483bdbf2a49bf60adfc98030ef0482fe2
Amoako10 <eamoako89@ya89@yahoo.com>
2022-11-04 15:16:32 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,42 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  main - Prints coints left
- *  @args: Paramater Entry
- *  @argv: One number of argumets
- *  Return: zero onOne number of argumetsr
+ * main - returns the min numbers of coins per given ammount of money.
+ *  @argc: argument count or size of arguments.
+ *  @argv: argument vector
+ *  @argv: argument vector
  */
-int main(int args, char *argv[])
+int main(int argc, char **argv)
 {
-	int  c, coins = 0;
+	int coins, i, ammount;
+	int value[5] = {25, 10, 5, 2, 1};
 
-	if (args != 2)
+	coins = 0;
+	ammount = atoi(argv[argc - 1]);
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	c = atoi(argv[1]);
-	if (c < 0)
+	else if (ammount <= 0)
 	{
 		printf("0\n");
-		return (0);
 	}
-	for (; c >= 0;)
+	else
 	{
-		if (c >= 25)
-			c -= 25;
-		else if (c >= 10)
-			c -= 10;
-		else if (c >= 5)
-			c -= 5;
-		else if (c >= 2)
-			c -= 2;
-		else if (c >= 1)
-			c -= 1;
-		else
-			break;
-		coins += 1;
+		for (i = 0; i < 5; i++)
+		{
+			if (value[i] <= ammount)
+			{
+				coins = coins + (ammount / value[i]);
+				ammount = ammount - (ammount / value[i]) * value[i];
+				if (ammount == 0)
+				{
+				printf("%d\n", coins);
+				break;
+				}
+			}
+		}
 	}
-	printf("%d\n", coins);
 	return (0);
 }
