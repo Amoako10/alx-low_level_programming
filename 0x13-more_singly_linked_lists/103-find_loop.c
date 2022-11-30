6c28c6633635fbb41448ab6c908d8317b7075537
Amoako10 <eamoako89@ya89@yahoo.com>
2022-11-30 15:00:40 -0800

    task 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,49 +3,33 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * main - check the code for Holberton School students.
- *  Return: Always 0.
+ * find_listint_loop - finds address of starting node in looped linked list
+ * @head: pointer to the head in linked list to test
+ * Return: The address of the node where the loop starts,
+ *  or NULL if there is no loop
  */
-int main(void)
+listint_t *find_listint_loop(listint_t *head)
 {
-	 listint_t *head;
-	 listint_t *head2;
-	 listint_t *node;
+	listint_t *hare, *tortoise;
 
-	 head2 = NULL;
-	 add_nodeint(&head2, 0);
-	 add_nodeint(&head2, 1);
-     	 add_nodeint(&head2, 2);
-         add_nodeint(&head2, 3);
-   	 add_nodeint(&head2, 4);
-         add_nodeint(&head2, 98);
-	 add_nodeint(&head2, 402);
-	 add_nodeint(&head2, 1024);
-	 print_listint_safe(head2);
-	 node = find_listint_loop(head2);
-	 if (node != NULL)
-         {
-          printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-	 }
-          free_listint_safe(&head2);
-	  head = NULL;
-	  node = add_nodeint(&head, 0);
-	  add_nodeint(&head, 1);
-	  add_nodeint(&head, 2);
-	  add_nodeint(&head, 3);
-	  add_nodeint(&head, 4);
-	  add_nodeint(&head, 5);
-          add_nodeint(&head, 6);
-	  node->next = add_nodeint(&head, 7);
-          add_nodeint(&head, 98);
-	  add_nodeint(&head, 402);
-          add_nodeint(&head, 1024);
-	  print_listint_safe(head);
-          node = find_listint_loop(head);
-	  if (node != NULL)
-	  {
-		  printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-	  }
-	   free_listint_safe(&head);
-	   return (0);
+	if (!head)
+		return (NULL);
+	hare = tortoise = head;
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+		if (hare == tortoise)
+		{
+			tortoise = head;
+			while (tortoise && hare)
+			{
+				if (tortoise == hare)
+					return (tortoise);
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+		}
+	}
+	return (NULL);
 }
