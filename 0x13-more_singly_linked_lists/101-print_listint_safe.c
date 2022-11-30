91219b448cb5420baca59ad1554ca3a20de5f0a0
Amoako10 <eamoako89@ya89@yahoo.com>
2022-11-30 15:11:35 -0800

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,8 +18,11 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		tmp2 = tmp;
 		tmp = tmp->next;
+
 		count++;
+
 		printf("[%p] %d\n", (void *)tmp2, tmp2->n);
+
 		if (tmp2 <= tmp)
 		{
 			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
