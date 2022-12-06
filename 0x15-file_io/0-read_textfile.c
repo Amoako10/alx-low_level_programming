5aeb71ed72338b03eec7534d0185e0bbf80e8e33
Amoako10 <eamoako89@ya89@yahoo.com>
2022-12-06 08:31:02 -0800

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,35 +3,30 @@
 #include "main.h"
 
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letters
- * Description: Write a function that reads a text file and prints it to the POSIX  standard output
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to open it
+ * @letters: The numbers of bytes to read
+ * Return: Number of bytes to read and write to the output
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+	int fd;
+	char *buf;
+	int lenRead, lenWrite;
 
-	text = malloc(letters);
-	if (text == NULL)
-		return (0);
 	if (filename == NULL)
 		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-	{
-		free(text);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	}
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
-
-	close(file);
-	return (w);
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
+	lenRead = read(fd, buf, letters);
+	lenWrite = write(STDOUT_FILENO, buf, lenRead);
+	if (lenWrite != lenRead && lenWrite == -1)
+		return (0);
+	free(buf);
+	close(fd);
+	return (lenRead);
 }
